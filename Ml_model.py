b8032d62b7e7e9bafc56579204314696f00c6202
Céline YE <celine.ye96@gmail.com>
2021-12-09 11:27:56 +0100

    test vader with sentences manually

--- Ml_model.py
+++ Ml_model.py
@@ -2,10 +2,27 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 
 
+
 def main():
-    classifier = SentimentIntensityAnalyzer()
-    #
+    #test some sentences 
+    sid_obj = SentimentIntensityAnalyzer()
+    sentiment_dict = sid_obj.polarity_scores("i'm so happy")
+    print("sentence was rated as", sentiment_dict['neg']*100,"% Negative")
+    print("sentence was rated as", sentiment_dict['neu']*100,"% Neutral")
+    print("sentence was rated as", sentiment_dict['pos']*100,"% Positive")
+    print("sentence Overall Rated As", end= " ")
+
+    if sentiment_dict['compound'] >= 0.05 :
+        print("Positive")
+    elif sentiment_dict['compound'] >= 0.05 :
+        print("Negative")
+    else :
+        print("Neutral")
 
 
 if __name__ == "__main__" :
     main()
+
+
+
+
