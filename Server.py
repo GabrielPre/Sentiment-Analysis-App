56fe0efc62775e97e57dec89eaf3c127d3397c90
Céline YE <celine.ye96@gmail.com>
2021-12-10 13:22:07 +0100

    Removed useless dependency on Redis

--- Server.py
+++
@@ -1,6 +0,0 @@
-import time
-import redis
-from flask import Flask
-
-app = Flask(__name__)
-cache = redis.Redis(host='redis', port=5000)
--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-import redis
 from flask import Flask
 app = Flask(__name__)
-cache = redis.Redis(host = 'redis', port = 6379)
 
 
 @app.route('/')
