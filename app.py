764e4f440fa8cd6004196e065c5afb8dc5e36e44
Céline YE <celine.ye96@gmail.com>
2021-12-10 13:42:53 +0100

    Added a way to show/remove the result div

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
 with open('frontend.html') as f:
     frontend_template = f.read()
 
+def prepare_frontend_template():
+    return frontend_template
+
 @app.route('/')
 def hello():
-
-    return frontend_template
+    return prepare_frontend_template()
