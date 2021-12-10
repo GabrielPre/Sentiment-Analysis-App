f8e3c63bd98a3e273b7a438e449ba2d0a4e8bd08
Céline YE <celine.ye96@gmail.com>
2021-12-10 13:23:38 +0100

    Moved the frontend to a separate file

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+with open('frontend.html') as f:
+    frontend_template = f.read()
 
 @app.route('/')
 def hello():
 
-    return  '''
-                <h1>Hello world</h1>
-                <a> Enter your phrase to analyse</a>
-                <input type="text" id="text_id" placeholder="text">
-                <button type="button" onclick="analyse()"> Analyse </button>
-                
-                <script> 
-                function analyse() {
-                    var x = document.getElementById("text_id").value;
-                    alert("You have entered "+x);
-                }
-                </script>'''
+    return frontend_template
