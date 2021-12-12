d8ae68e592c3781ce92586ca658363589db0a499
Gabriel <gabriel.precigout@gmail.com>
2021-12-12 23:45:28 +0100

    Adding correlation matrix to test accuracy

--- Ml_model.py
+++ Ml_model.py
@@ -25,12 +25,13 @@ def get_sentiment_from_compound(compound_score):
 
 
 def test_model_accuracy():
-    with open("dataset.txt") as file:
-        for line in file.readlines():
-            id, compound, text = line.strip().split('\t')
+    
+    conf_vals = pd.read_csv('dataset.csv')
+    conf_vals = conf_vals.Conf_Matrix.value_counts().to_dict()
+    print(conf_vals)
 
-            sentiment = get_sentiment_from_compound(float(compound))
-            predicted_sentiment = predict(text)
+    accuracy = (conf_vals['TP'] + conf_vals['TN']) / (conf_vals['TP'] + conf_vals['TN'] + conf_vals['FP'] + conf_vals['FN'])
+    print('Accuracy: ', round(100 * accuracy, 2),'%')
 
 def conf_matrix(x):
     if x[1] == 1 and x[2] == 1:
@@ -45,40 +46,27 @@ def conf_matrix(x):
         return 0
 
 def create_dataset():
-    (train_data_raw, train_labels), (test_data_raw, test_labels) = tf.keras.datasets.imdb.load_data(index_from=3)
-    words2idx = tf.keras.datasets.imdb.get_word_index()
-    idx2words = {idx:word for word, idx in words2idx.items()}
-    print('yes')
-    train_ex = [idx2words[x-3] for x in train_data_raw[0][1:]] # We use x-3 because when we load the data above, we used index_form=3
-    train_ex = ' '.join(train_ex)
-    print(train_ex)
+    #
+    (train_data, train_labels), (test_data, test_labels) = tf.keras.datasets.imdb.load_data(index_from=3)
+    words2index = tf.keras.datasets.imdb.get_word_index()
+    index2words = {index:word for word, index in words2index.items()}
     imdb_reviews = []
-    for review, label in zip(train_data_raw, train_labels):
+    for review, label in zip(train_data, train_labels):
         try:
-            tokens = [idx2words[x-3] for x in review[1:]]
+            tokens = [index2words[x-3] for x in review[1:]]
             text = ' '.join(tokens)
             imdb_reviews.append([text, label])
         except: # There is a distorted observation. For that, we need to handle the error
             print('Small index number')
             pass
     imdb_df = pd.DataFrame(imdb_reviews,columns=['Text', 'Label'])
-    print(imdb_df.info())
-    print('fdp')
-    print(imdb_df.head(10))
-    print('lessgo')
     imdb_slice = imdb_df
-    print('lessgo1?')
     imdb_slice['Prediction'] = imdb_slice['Text'].apply(lambda x: 1 if sid_obj.polarity_scores(x)['compound'] >= 0 else -1)
-    print("stress test 0")
     # Edit Label column 1 for Positive, -1 for Negative
     imdb_slice['Label'] = imdb_slice['Label'].apply(lambda x: -1 if x == 0 else 1)
-    print("stress test 1")
     # Check if the Label column and Prediction column match for accuracy calculation
     imdb_slice['Accuracy'] = imdb_slice.apply(lambda x: 1 if x[1] == x[2] else 0, axis=1)
-    print("stress test 2")
     imdb_slice['Conf_Matrix'] = imdb_slice.apply(lambda x: conf_matrix(x), axis=1)
-    print("stress test 3")
-    print(imdb_slice.tail(10))
     imdb_slice.to_csv('dataset.csv',index=False, encoding='utf-8')
 
 
@@ -86,7 +74,7 @@ def create_dataset():
 
 def main():
     #test with a dataset
-    create_dataset()
+    test_model_accuracy()
 
 
 
