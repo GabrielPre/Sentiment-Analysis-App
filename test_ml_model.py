bd10d8ae16737fbb42338f61346443a236438091
Céline YE <celine.ye96@gmail.com>
2021-12-13 14:06:39 +0100

    Renamed Tests.py to test_app.py

---
+++ test_ml_model.py
@@ -0,0 +1,11 @@
+import pytest
+import Ml_model
+
+
+def test_sentiment():
+    assert Ml_model.predict("I love sushis") == "positive"
+    assert Ml_model.predict("There is a dog") == "neutral"
+    assert Ml_model.predict("I hate this food") == "negative"
+
+def test_accuracy():
+    assert Ml_model.test_model_accuracy() >= 0.8, "vader accuracy is < 0.8"
