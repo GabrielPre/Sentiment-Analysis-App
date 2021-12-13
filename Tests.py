030e9d1f39b05a1386b703d6fe8eea585867acfa
Céline YE <celine.ye96@gmail.com>
2021-12-13 13:02:06 +0100

    Fixed unit test not working

--- Tests.py
+++ Tests.py
@@ -3,9 +3,9 @@ import Ml_model
 
 
 def test_sentiment():
-    assert Ml_model.predict("I love sushis") == "Positive"
-    assert Ml_model.predict("There is a dog") == "Neutral"
-    assert Ml_model.predict("I hate this food") == "Negative"
+    assert Ml_model.predict("I love sushis") == "positive"
+    assert Ml_model.predict("There is a dog") == "neutral"
+    assert Ml_model.predict("I hate this food") == "negative"
 
 def test_accuracy():
     assert Ml_model.test_model_accuracy() >= 0.8, "vader accuracy is < 0.8"
