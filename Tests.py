027e8c5ba646906c4eff608a4bbd1acc18c1eee5
Gabriel <gabriel.precigout@gmail.com>
2021-12-13 12:33:24 +0100

    Update tests

--- Tests.py
+++ Tests.py
@@ -3,9 +3,9 @@ import Ml_model
 
 
 def test_sentiment():
-    assert Ml_model.predict("I love cake") == "Positive"
-    assert Ml_model.predict("There is a cat") == "Neutral"
-    assert Ml_model.predict("I hate this project") == "Negative"
+    assert Ml_model.predict("I love sushis") == "Positive"
+    assert Ml_model.predict("There is a dog") == "Neutral"
+    assert Ml_model.predict("I hate this food") == "Negative"
 
 def test_accuracy():
-    assert Ml_model.test_model_accuracy() >= 0.6, "vader accuracy is < 0.6"
+    assert Ml_model.test_model_accuracy() >= 0.8, "vader accuracy is < 0.8"
